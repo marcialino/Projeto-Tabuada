e04ac422b8272f4d8ba4ddf12b71c22cabf851ae
marcialino <marcialino100@gmail.com>
2023-03-12 20:05:54 -0300

    Atualizei projeto-tabuada

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert('Olá!')
+function calcular (){
+
+var n = window.document.getElementById('num')
+var tab = window.document.getElementById('tab')
+
+    if (n.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    }else{
+        n = Number(n.value)
+        var c = 1
+        tab.innerHTML = ''
+
+        while(c <= 10 ){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+
+        }
+    }
+
+}
+
+
+
