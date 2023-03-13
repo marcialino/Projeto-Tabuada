70b1456e88157af95e79e2a75df7caa69a2ef036
marcialino <marcialino100@gmail.com>
2023-03-13 17:07:30 -0300

    Atualizei

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ var tab = window.document.getElementById('tab')
         window.alert('Por favor, digite um número!')
     }else{
         n = Number(n.value)
-        var c = 1
         tab.innerHTML = ''
 
-        while(c <= 10 ){
+        /*while(c <= 10 ){
             var item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
-            c++
+            c++*/
 
+        for(var c = 1; c <=10; c++){
+            var item = document.createElement('option')
+            item.text += `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
         }
-    }
+
+   }
 
 }
-
-
-
